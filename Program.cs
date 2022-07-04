cabdbb6c2dc362cd2d0cf9442e705127da2375cf
Vladislav <vladislav.v.o@yandex.ru>
2022-07-04 20:30:54 +0300

    скорректировал решение 1й задачи

--- Program.cs
+++ Program.cs
@@ -8,15 +8,12 @@
 /*
 int sumOfDigits(int number1)
 {
-int digitCount = (int)Math.Log10(number1) + 1;
-int digit = digitCount;
-int sum = 0;
-for (int i=1; i<=digitCount; i++)
+int digitCount = (int)Math.Log10(number1);
+    int sum = 0;
+    for (int i=1; i<=digitCount+1; i++)
     {
-    int StepenDeleniya = Convert.ToInt32(Math.Pow(10, digit-1));
-    sum = sum + number1 / StepenDeleniya;
-    number1 = number1 % StepenDeleniya;
-    digit = digit - 1;
+        sum = sum + number1 % 10;
+        number1 = number1 / 10;
     }
 return sum;
 }
@@ -25,8 +22,22 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Sum of digits is " + sumOfDigits(number1));
 */
 
+
 /*
 Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
+
+
+int y = 5;
+int[] array = new int[y];
+Random rand = new Random();
+
+for (int i = 0; i < y; i++)
+{
+     array[i] = rand.Next(1, 21);
+    Console.Write(array[i] + " ");
+
+}
+
